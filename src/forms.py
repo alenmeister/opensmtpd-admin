635f12020ebd66b9e65128d050462f3ec5face02
Alen Mistric <alen@mistric.no>
2024-02-25 21:35:21 +0100

    Remove regex and move EqualTo validator in field for new password

--- src/forms.py
+++ src/forms.py
@@ -2,7 +2,7 @@
 
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class LoginForm(FlaskForm):
@@ -26,25 +26,15 @@ class UpdateForm(FlaskForm):
 
     current_password = PasswordField('Current password', validators=[DataRequired()])
 
+    # TODO: Add regex validation
     new_password = PasswordField(
         'New password',
         validators=[
             DataRequired(),
             Length(min=10, message='Password must be 10 characters or longer'),
-            # TODO: Tidy up messy regex expression
-            Regexp(
-                '(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$',
-                message='Password must contain at least one uppercase, one lowercase and one number'
-            )
+            EqualTo('confirm_password', message='Passwords must match')
         ]
     )
 
-    confirm_password = PasswordField(
-        'Confirm Password',
-        validators=[
-            DataRequired(),
-            EqualTo('new_password', message='Passwords must match')
-        ]
-    )
-
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Update password')
