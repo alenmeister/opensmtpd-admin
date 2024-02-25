bbc92ce25c56e3cc031e3794743461b6450624e6
Alen Mistric <alen@mistric.no>
2024-02-25 18:59:59 +0100

    Add config constants for assets debug

--- config.py
+++ config.py
@@ -7,20 +7,26 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
+local_sqlite_db = 'sqlite:///' + path.join(basedir, 'opensmtpd.db')
+
+
 class Config:
+    # pylint: disable=too-few-public-methods
     """Set app configuration from environment variables."""
 
     ENVIRONMENT = environ.get('ENVIRONMENT')
 
-    FLASK_APP = 'app.py'
+    FLASK_APP = 'wsgi.py'
     FLASK_DEBUG = environ.get('FLASK_DEBUG')
     SECRET_KEY = environ.get('SECRET_KEY')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI') or local_sqlite_db
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Assets
+    # Static assets
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
+    ASSETS_DEBUG = False
+    COMPRESSOR_DEBUG = False
