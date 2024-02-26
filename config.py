35cf678c8dac1c00bd7b23c6d206aba51b71d21c
Alen Mistric <alen@mistric.no>
2024-02-26 21:17:45 +0100

    Convert all constants to uppercase

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from os import environ, path
 
 from dotenv import load_dotenv
 
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, '.env'))
 
-local_sqlite_db = 'sqlite:///' + path.join(basedir, 'opensmtpd.db')
+SQLITE_DB = 'sqlite:///' + path.join(BASE_DIR, 'opensmtpd.db')
 
 
 class Config:
@@ -21,7 +21,7 @@ class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI') or local_sqlite_db
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI') or SQLITE_DB
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
