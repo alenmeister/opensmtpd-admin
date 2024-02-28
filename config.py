2d16571557deeab7ae6a850f8f25eefd1583bf51
Alen Mistric <alen@mistric.no>
2024-02-28 20:37:47 +0100

    Remove SQLITE_DB constant from config

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ from dotenv import load_dotenv
 BASE_DIR = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(BASE_DIR, '.env'))
 
-SQLITE_DB = 'sqlite:///' + path.join(BASE_DIR, 'opensmtpd.db')
-
 
 class Config:
     """Set configuration from environment variables"""
@@ -20,7 +18,7 @@ class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI') or SQLITE_DB
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
