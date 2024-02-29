7b30f08ba53e87e7bf49cdd50397a2c9828ecab2
Alen Mistric <alen@mistric.no>
2024-02-29 19:55:13 +0100

    Update tests for development and production configurations

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,9 +1,19 @@
 """Unit tests for environment config settings"""
 
+from flask import Flask
 
-def test_development_config(flask_app):
+
+def test_development_config(flask_app: Flask):
+    assert flask_app.config['ENVIRONMENT'] == 'development'
     assert flask_app.config['SECRET_KEY'] != 'Foo?Bar!'
+    assert not flask_app.config['TESTING']
+    assert flask_app.config['FLASK_DEBUG'] == 'True'
+    assert flask_app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite://')
 
 
-def test_production_config(flask_app):
+def test_production_config(flask_app: Flask):
+    assert flask_app.config['ENVIRONMENT'] == 'production'
     assert flask_app.config['SECRET_KEY'] != 'Foo?Bar!Baz?!'
+    assert not flask_app.config['TESTING']
+    assert flask_app.config['FLASK_DEBUG'] == 'False'
+    assert flask_app.config['SQLALCHEMY_DATABASE_URI'].startswith('mysql+pymysql://')
